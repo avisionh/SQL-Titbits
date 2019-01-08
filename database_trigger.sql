8be04f6c5b5d36fdcfc1ced0a16172a0af578fce
Avision Ho <avision.ho@education.gov.uk>
2019-01-08 12:20:23 +0000

    chore: update file name and references to follow style guide.

--- database_trigger.sql
+++ database_trigger.sql
@@ -1,15 +1,15 @@
 USE [<database_name>]
 GO
 
-/****** Object:  Table [dbo].[DDLChangeLog]    Script Date: 08/01/2019 12:09:33 ******/
+/****** Object:  Table [dbo].[DatabaseChangeLog]    Script Date: 08/01/2019 12:09:33 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[DDLChangeLog](
-	[DDLChangeLog_ID] [int] IDENTITY(1,1) NOT NULL,
+CREATE TABLE [dbo].[DatabaseChangeLog](
+	[DatabaseChangeLog_ID] [int] IDENTITY(1,1) NOT NULL,
 	[InsertionDate] [datetime] NOT NULL,
 	[CurrentUser] [nvarchar](50) NOT NULL,
 	[LoginName] [nvarchar](50) NOT NULL,
@@ -21,16 +21,16 @@ CREATE TABLE [dbo].[DDLChangeLog](
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[DDLChangeLog] ADD  CONSTRAINT [DF_ddl_log_InsertionDate]  DEFAULT (getdate()) FOR [InsertionDate]
+ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChangeLog_InsertionDate]  DEFAULT (getdate()) FOR [InsertionDate]
 GO
 
-ALTER TABLE [dbo].[DDLChangeLog] ADD  CONSTRAINT [DF_ddl_log_CurrentUser]  DEFAULT (CONVERT([nvarchar](50),user_name(),(0))) FOR [CurrentUser]
+ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChange_CurrentUser]  DEFAULT (CONVERT([nvarchar](50),user_name(),(0))) FOR [CurrentUser]
 GO
 
-ALTER TABLE [dbo].[DDLChangeLog] ADD  CONSTRAINT [DF_DDLChangeLog_LoginName]  DEFAULT (CONVERT([nvarchar](50),suser_sname(),(0))) FOR [LoginName]
+ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChangeLog_LoginName]  DEFAULT (CONVERT([nvarchar](50),suser_sname(),(0))) FOR [LoginName]
 GO
 
-ALTER TABLE [dbo].[DDLChangeLog] ADD  CONSTRAINT [DF_DDLChangeLog_Username]  DEFAULT (CONVERT([nvarchar](50),original_login(),(0))) FOR [Username]
+ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChangeLog_Username]  DEFAULT (CONVERT([nvarchar](50),original_login(),(0))) FOR [Username]
 GO
 
 
