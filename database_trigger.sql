be98c0c68f2125df612e4efab5dcaeeea417715c
Avision Ho <avision.ho@education.gov.uk>
2019-01-08 17:50:21 +0000

    refactor: change names to be clearer to SQL function used.
    This is so it is more readable and consistent with the functions that generate it.

--- database_trigger.sql
+++ database_trigger.sql
@@ -1,19 +1,19 @@
 USE [<database_name>]
 GO
 
-/****** Object:  Table [dbo].[DatabaseChangeLog]    Script Date: 08/01/2019 12:09:33 ******/
+/****** Object:  Table [AdminDetails].[DatabaseChangeLog]    Script Date: 08/01/2019 12:09:33 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[DatabaseChangeLog](
-	[DatabaseChangeLog_ID] [int] IDENTITY(1,1) NOT NULL,
-	[InsertionDate] [datetime] NOT NULL,
-	[CurrentUser] [nvarchar](50) NOT NULL,
+CREATE TABLE [AdminDetails].[DatabaseChangeLog](
+	[ChangeId] [int] IDENTITY(1,1) NOT NULL,
+	[ChangeDate] [datetime] NOT NULL,
+	[UserName] [nvarchar](50) NOT NULL,
+	[SecurityName] [nvarchar](50) NOT NULL,
 	[LoginName] [nvarchar](50) NOT NULL,
-	[Username] [nvarchar](50) NOT NULL,
 	[EventType] [nvarchar](100) NULL,
 	[ObjectName] [nvarchar](100) NULL,
 	[ObjectType] [nvarchar](100) NULL,
@@ -21,16 +21,14 @@ CREATE TABLE [dbo].[DatabaseChangeLog](
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 GO
 
-ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChangeLog_InsertionDate]  DEFAULT (getdate()) FOR [InsertionDate]
+ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChangeLog_ChangeDate]  DEFAULT (getdate()) FOR [ChangeDate]
 GO
 
-ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChange_CurrentUser]  DEFAULT (CONVERT([nvarchar](50),user_name(),(0))) FOR [CurrentUser]
+ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChangeLog_UserName]  DEFAULT (CONVERT([nvarchar](50),user_name(),(0))) FOR [CurrentUser]
 GO
 
-ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChangeLog_LoginName]  DEFAULT (CONVERT([nvarchar](50),suser_sname(),(0))) FOR [LoginName]
+ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChangeLog_SecurityName]  DEFAULT (CONVERT([nvarchar](50),suser_sname(),(0))) FOR [LoginName]
 GO
 
-ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChangeLog_Username]  DEFAULT (CONVERT([nvarchar](50),original_login(),(0))) FOR [Username]
+ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChangeLog_LoginName]  DEFAULT (CONVERT([nvarchar](50),original_login(),(0))) FOR [Username]
 GO
-
-
