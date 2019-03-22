52f272840f095b7650a7c961f0ecee18784b375e
Avision Ho <avision.ho@education.gov.uk>
2019-03-22 17:25:27 +0000

    fix: change table definition to set defaults and constraints directly.
    Before, it existed as separate lines of code below. e.g. 'ALTER TABLE ...', but this doesn't work when initialising the table, so need to take current approach.

--- Database Trigger/table_databasetrigger.sql
+++ Database Trigger/table_databasetrigger.sql
@@ -1,34 +1,33 @@
-USE [<database_name>]
+USE [HE-AN-DATA-NS]
 GO
 
-/****** Object:  Table [AdminDetails].[DatabaseChangeLog]    Script Date: 08/01/2019 12:09:33 ******/
+/****** Object:  Table [AuditDetails].[DatabaseChangeLog]    Script Date: 22/03/2019 16:10:38 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [AdminDetails].[DatabaseChangeLog](
-	[ChangeId] [int] IDENTITY(1,1) NOT NULL,
-	[ChangeDate] [datetime] NOT NULL,
-	[NameUser] [nvarchar](50) NOT NULL,
-	[NameSecurity] [nvarchar](50) NOT NULL,
-	[NameLogin] [nvarchar](50) NOT NULL,
-	[EventType] [nvarchar](100) NULL,
-	[ObjectName] [nvarchar](100) NULL,
-	[ObjectType] [nvarchar](100) NULL,
-	[TsqlCode] [nvarchar](max) NULL
+CREATE TABLE [AuditDetails].[DatabaseChangeLog](
+	[ChangeId] [int] IDENTITY(1,1) NOT NULL
+	,[ChangeDate] [datetime] NOT NULL 
+		CONSTRAINT [DF_ddl_log_ChangeDate]
+			DEFAULT (GETDATE())
+	,[NameUser] [nvarchar](50) NOT NULL
+		CONSTRAINT [DF_ddl_log_NameUser]   
+            DEFAULT (CONVERT([nvarchar](50), USER_NAME(), (0)))
+	,[NameSecurity] [nvarchar](50) NOT NULL
+		CONSTRAINT [DF_DDLChangeLog_NameSecurity]   
+            DEFAULT (CONVERT([nvarchar](50), SUSER_SNAME(), (0)))
+	,[NameLogin] [nvarchar](50) NOT NULL
+		CONSTRAINT [DF_DDLChangeLog_NameLogin]   
+            DEFAULT (CONVERT([nvarchar](50), original_login(),(0)))
+	,[EventType] [nvarchar](100) NULL
+	,[ObjectName] [nvarchar](100) NULL
+	,[ObjectType] [nvarchar](100) NULL
+	,[TsqlCode] [nvarchar](max) NULL
 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
 GO
 
-ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChangeLog_ChangeDate]  DEFAULT (getdate()) FOR [ChangeDate]
-GO
 
-ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChangeLog_UserName]  DEFAULT (CONVERT([nvarchar](50),user_name(),(0))) FOR [CurrentUser]
-GO
-
-ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChangeLog_SecurityName]  DEFAULT (CONVERT([nvarchar](50),suser_sname(),(0))) FOR [LoginName]
-GO
-
-ALTER TABLE [dbo].[DatabaseChangeLog] ADD  CONSTRAINT [DF_DatabaseChangeLog_LoginName]  DEFAULT (CONVERT([nvarchar](50),original_login(),(0))) FOR [Username]
-GO
